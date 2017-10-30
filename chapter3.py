8625a41c7d49d32ceb2cdd932d84a0c8a135e550
Nick K <ivankapalala@gmail.com>
2017-10-30 17:29:37 +0100

    Create chapter3.py

---
+++ chapter3.py
@@ -0,0 +1,94 @@
+invites = ['micheal jackon', 'micheal jordan','bob marley','2pac']
+
+invitati = invites
+
+print "Hello Mr. " + invites[0].title() + " would you like to dinner with me?"
+
+print "Hello his highness. " + invites[1].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[2].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[3].title() + " would you like to dinner with me?\n"
+
+print "Unfortunately Mr." + invites[1].title() + " could not make it"
+
+invites[1] = "john"
+
+print "Mr." + invites[1].title() + " will take his place"
+
+print "We just found a bigger dinner table more guest are expected \n"
+
+invites.insert(0, 'madonna')
+invites.insert(1, 'george micheal')
+invites.append('david bowie')
+
+print "Hello Miss " + invites[0].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[1].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[2].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[3].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[4].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[5].title() + " would you like to dinner with me?"
+
+print "Hello Mr. " + invites[6].title() + " would you like to dinner with me? \n"
+
+print "Im sorry i can only invite 2 person tonight\n"
+
+print invites.pop(0).title() + " i'm sorry you're no longer invited"
+
+print invites.pop(0).title() + " i'm sorry you're no longer invited"
+
+print invites.pop(1).title() + " i'm sorry you're no longer invited"
+
+print invites.pop(2).title() + " i'm sorry you're no longer invited"
+
+print invites.pop(2).title() + " i'm sorry you're no longer invited\n"
+
+print "Mr " + invites[0].title() + " you're still invited!"
+
+print "Mr " + invites[1].title() + " you're still invited!"
+
+del invites[0]
+del invites[0]
+
+lystr = "the gust list is: " + str.invites
+
+print lystr
+
+
+print "Here :" + invits
+
+
+
+cities = ['Tokyo', 'Los Angeles','New York','Atlanta']
+
+print cities
+
+print sorted(cities)
+print cities
+
+
+print sorted(cities ,reverse=True)
+
+print cities
+
+cities.reverse()
+print cities
+
+cities.reverse()
+print cities
+
+cities.sort()
+print cities
+
+cities.sort(reverse=True)
+print cities
+
+invites = ['micheal jackon', 'micheal jordan','bob marley','2pac']
+
+invitati = len(invites)
+print "We're having: " + str(invitati) + " guest tonight!"
